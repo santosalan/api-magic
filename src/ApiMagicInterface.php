b5b199ddc6de2e150fc8289d37e3be3ce106bd4c
Unknown <malk0704@gmail.com>
2018-03-02 10:08:16 -0300

    Interface

--- src/ApiMagicInterface.php
+++ src/ApiMagicInterface.php
@@ -9,18 +9,6 @@ interface ApiMagicInterface
 
     public function __call ($name, $arguments = null);
     
-    private function setElement($element);
-    
-    private function getElement();
-
     public function element($element);
-
-    protected function proccess($name, $arguments);
-
-    protected function verifyRoute($route);
-
-    protected function apiRequest($url, $arguments, $element = null);
-
-    protected function token();
 
 }    
